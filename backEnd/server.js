1a9a2dec0c74259094783549d7b45b645273e254
Vivek <vivek.raj2024@nst.rishihood.edu.in>
2025-11-17 16:03:40 +0530

    Updated backend for Render deployment

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  }));
 app.use(express.json());
 
 connectDB();
